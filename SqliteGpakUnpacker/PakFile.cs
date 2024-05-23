95b0717ad9e2bfd5b4f27b097ed88f1b4aae2e3c
Crypto137 <Crypto137@users.noreply.github.com>
2024-05-23 15:48:49 +0300

    Implement SqliteGpakUnpacker tool

---
+++ SqliteGpakUnpacker/PakFile.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+namespace SqliteGpakUnpacker
+{
+    public class PakFile
+    {
+        private readonly Dictionary<string, PakEntry> _entryDict = new();
+
+        public int Count { get => _entryDict.Count; }
+
+        public void AddEntry(PakEntry entry)
+        {
+            _entryDict.Add(entry.Name, entry);
+        }
+
+        public void SaveToDisk(string outputDirectoryName)
+        {
+            string assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string outputDirectory = Path.Combine(assemblyDirectory, outputDirectoryName);
+
+            foreach (PakEntry entry in _entryDict.Values.OrderBy(entry => entry.Name))
+            {
+                string filePath = Path.Combine(outputDirectory, entry.Name);
+                string fileDirectory = Path.GetDirectoryName(filePath);
+                if (Directory.Exists(fileDirectory) == false)
+                    Directory.CreateDirectory(fileDirectory);
+
+                Console.WriteLine(entry.Name);
+                File.WriteAllBytes(filePath, entry.Blob);
+            }
+        }
+    }
+}
---
+++ SqliteGpakUnpacker/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SqliteGpakUnpacker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: SqliteGpakUnpacker.exe [filePath]");
+                return;
+            }
+
+            PakUnpacker.Unpack(args[0]);
+            Console.ReadKey();
+        }
+    }
+}
