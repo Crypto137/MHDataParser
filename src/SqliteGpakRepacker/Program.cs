e27ece53f6e7097bc8a2d1ab9be0f8005966b877
Crypto137 <Crypto137@users.noreply.github.com>
2025-03-17 07:43:43 +0300

    Remove the outdated SqliteGpakUnpacker
    Use SqliteGpakRepacker instead

--- src/SqliteGpakRepacker/Program.cs
+++ src/SqliteGpakRepacker/Program.cs
@@ -6,6 +6,8 @@
         {
             if (args.Length == 0)
             {
+                Console.WriteLine("SqliteGpakRepacker");
+                Console.WriteLine("Repacks legacy SQLite-based GPAK archives into the format used by later (1.29+) versions of the game.");
                 Console.WriteLine("Usage: SqliteGpakRepacker.exe [filePath] [optional outputDirectory]");
             }
             else
